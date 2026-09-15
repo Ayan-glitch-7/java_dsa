46fe804ba8ede085477a62ea2a49f5234fdf0590
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-15 21:33:23 +0530

    c

---
+++ assignments/07_sorting (assignment)/01.easy/Main_01.java
@@ -0,0 +1,33 @@
+/* LEETCODE (# 88) */
+
+public class Main_01 {
+  // public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+  //   for (int i = 0; i < n; i++) {
+  //     nums1[m + i] = nums2[i];
+  //   }
+
+  //   bubbleSort(nums1);
+  // }
+
+  // static void bubbleSort(int[] arr) {
+  //   boolean swapped;
+
+  //   for (int i = 0; i < arr.length; i++) {
+  //     swapped = false;
+
+  //     for (int j = 1; j < arr.length; j++) {
+  //       if (arr[j] < arr[j - 1]) {
+  //         int temp = arr[j];
+  //         arr[j] = arr[j - 1];
+  //         arr[j - 1] = temp;
+  //         swapped = true;
+  //       }
+  //     }
+
+  //     if (swapped == false) {
+  //       break;
+  //     }
+  //   }
+  // }
+}
