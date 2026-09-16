a425ed50c9ea3e266e7a4a9ad0c95f9c03867840
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-16 23:46:25 +0530

    c

---
+++ assignments/07_sorting (assignment)/01.easy/Main_18.java
@@ -0,0 +1,28 @@
+/* LEETCODE (# 1200) */
+
+// import java.util.ArrayList;
+// import java.util.Arrays;
+// import java.util.List;
+
+public class Main_18 {
+  // public List<List<Integer>> minimumAbsDifference(int[] arr) {
+
+  //   Arrays.sort(arr);
+  //   int min = arr[1] - arr[0];
+
+  //   for (int i = 1; i < arr.length; i++) {
+  //     if ((arr[i] - arr[i - 1]) < min) {
+  //       min = arr[i] - arr[i - 1];
+  //     }
+  //   }
+
+  //   List<List<Integer>> ans = new ArrayList<>();
+
+  //   for (int j = 1; j < arr.length; j++) {
+  //     if ((arr[j] - arr[j - 1]) == min) {
+  //       ans.add(Arrays.asList(arr[j - 1], arr[j]));
+  //     }
+  //   }
+  //   return ans;
+  // }
+}
