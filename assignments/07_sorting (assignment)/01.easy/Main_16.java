74b4d806db73585ca1135575da2db84be1b782b0
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-16 22:43:28 +0530

    c

---
+++ assignments/07_sorting (assignment)/01.easy/Main_16.java
@@ -0,0 +1,23 @@
+/* LEETCODE (# 1061) */
+
+// import java.util.Arrays;
+
+public class Main_16 {
+  // public int heightChecker(int[] heights) {
+
+  //   int[] expected = new int[heights.length];
+  //   int count = 0;
+
+  //   for (int i = 0; i < heights.length; i++) {
+  //     expected[i] = heights[i];
+  //   }
+  //   Arrays.sort(expected);
+
+  //   for (int j = 0; j < heights.length; j++) {
+  //     if (expected[j] != heights[j]) {
+  //       count++;
+  //     }
+  //   }
+  //   return count;
+  // }
+}
