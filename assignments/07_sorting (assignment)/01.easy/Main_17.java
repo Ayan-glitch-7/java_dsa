93a2ee047003acc115bfc6ee04c10b9cef1c5eda
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-16 23:30:08 +0530

    c

---
+++ assignments/07_sorting (assignment)/01.easy/Main_17.java
@@ -0,0 +1,31 @@
+/* LEETCODE (# 1122) */
+
+// import java.util.Arrays;
+
+public class Main_17 {
+  // public int[] relativeSortArray(int[] arr1, int[] arr2) {
+
+  //   int m = arr1.length;
+  //   int n = arr2.length;
+
+  //   Arrays.sort(arr1);
+  //   int[] ans = new int[m];
+  //   int k = 0;
+
+  //   for (int i = 0; i < n; i++) {
+  //     for (int j = 0; j < m; j++) {
+  //       if (arr1[j] == arr2[i]) {
+  //         ans[k++] = arr1[j];
+  //         arr1[j] = -1;
+  //       }
+  //     }
+  //   }
+
+  //   for (int i = 0; i < arr1.length; i++) {
+  //     if (arr1[i] != -1) {
+  //       ans[k++] = arr1[i];
+  //     }
+  //   }
+  //   return ans;
+  // }
+}
