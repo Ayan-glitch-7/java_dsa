07ffcd5e09f3a5500f30608f2d5ecfb95844899b
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-18 15:16:41 +0530

    c

---
+++ assignments/05_arrays (assignment)/03.hard/Main_02.java
@@ -0,0 +1,18 @@
+/* LEETCODE (# 41) */
+
+// import java.util.Arrays;
+
+public class Main_02 {
+  // public int firstMissingPositive(int[] nums) {
+
+  //   Arrays.sort(nums);
+  //   int value = 1;
+
+  //   for (int i = 0; i < nums.length; i++) {
+  //     if (nums[i] == value) {
+  //       value++;
+  //     }
+  //   }
+  //   return value;
+  // }
+}
