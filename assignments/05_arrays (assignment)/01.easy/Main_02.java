2f16bb4534e9542cb99763b7cb2e8a95c1fcc258
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-19 05:50:05 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_02.java
+++ assignments/05_arrays (assignment)/01.easy/Main_02.java
@@ -1,14 +1,14 @@
 /* LEETCODE (# 1929) */
 
 class Main_02 {
-    // public int[] getConcatenation(int[] nums) {
-    //     int n = nums.length;
-    //     int[] ans = new int[2 * n];
+  // public int[] getConcatenation(int[] nums) {
+  //   int n = nums.length;
+  //   int[] ans = new int[2 * n];
 
-    //     for (int i = 0; i < n; i++) {
-    //         ans[i] = nums[i];
-    //         ans[i + n] = nums[i];
-    //     }
-    //     return ans;
-    // }
+  //   for (int i = 0; i < n; i++) {
+  //     ans[i] = nums[i];
+  //     ans[i + n] = nums[i];
+  //   }
+  //   return ans;
+  // }
 }
