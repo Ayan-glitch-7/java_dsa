9fafdc57e994131aeac95036ef474bc1c97b5a01
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-19 05:50:13 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_03.java
+++ assignments/05_arrays (assignment)/01.easy/Main_03.java
@@ -1,14 +1,14 @@
 /* LEETCODE (# 1480) */
 
 class Main_03 {
-    // public int[] runningSum(int[] nums) {
+  // public int[] runningSum(int[] nums) {
 
-    //     int[] runningSum = new int[nums.length];
-    //     runningSum[0] = nums[0];
+  //   int[] runningSum = new int[nums.length];
+  //   runningSum[0] = nums[0];
 
-    //     for (int i = 1; i < nums.length; i++) {
-    //         runningSum[i] = runningSum[i - 1] + nums[i];
-    //     }
-    //     return runningSum;
-    // }
+  //   for (int i = 1; i < nums.length; i++) {
+  //     runningSum[i] = runningSum[i - 1] + nums[i];
+  //   }
+  //   return runningSum;
+  // }
 }
