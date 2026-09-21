102974238e9ab97f49cd17d839c269cf2d2a3427
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-21 18:20:11 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_04.java
+++ assignments/05_arrays (assignment)/01.easy/Main_04.java
@@ -1,19 +1,19 @@
 /* LEETCODE (# 1672) */
 
 class Main_04 {
-    // public int maximumWealth(int[][] accounts) {
+  public int maximumWealth(int[][] accounts) {
 
-    //     int wealth = 0;
+    int wealth = 0;
 
-    //     for (int i = 0; i < accounts.length; i++) {
-    //         int money = 0;
-    //         for (int j = 0; j < accounts[i].length; j++) {
-    //             money = money + accounts[i][j];
-    //         }
-    //         if (money > wealth) {
-    //             wealth = money;
-    //         }
-    //     }
-    //     return wealth;
-    // }
+    for (int i = 0; i < accounts.length; i++) {
+      int money = 0;
+      for (int j = 0; j < accounts[i].length; j++) {
+        money = money + accounts[i][j];
+      }
+      if (money > wealth) {
+        wealth = money;
+      }
+    }
+    return wealth;
+  }
 }
