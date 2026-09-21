51879d0c29186ff94b957d4969fbfd69184e5b89
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-21 18:20:29 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_05.java
+++ assignments/05_arrays (assignment)/01.easy/Main_05.java
@@ -1,14 +1,14 @@
 /* LEETCODE (# 1470) */
 
 class Main_05 {
-    // public int[] shuffle(int[] nums, int n) {
+  // public int[] shuffle(int[] nums, int n) {
 
-    //     int[] ans = new int[nums.length];
+  //   int[] ans = new int[nums.length];
 
-    //     for (int i = 0; i < nums.length / 2; i++) {
-    //         ans[2 * i] = nums[i];
-    //         ans[2 * i + 1] = nums[n + i];
-    //     }
-    //     return ans;
-    // }
+  //   for (int i = 0; i < nums.length / 2; i++) {
+  //     ans[2 * i] = nums[i];
+  //     ans[2 * i + 1] = nums[n + i];
+  //   }
+  //   return ans;
+  // }
 }
