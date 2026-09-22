b866a0d2d2157b4dd2a58070105b6fb3b8476949
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-22 15:17:35 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_07.java
+++ assignments/05_arrays (assignment)/01.easy/Main_07.java
@@ -1,17 +1,17 @@
 /* LEETCODE (# 1512) */
 
 class Main_07 {
-//     public int numIdenticalPairs(int[] nums) {
+  // public int numIdenticalPairs(int[] nums) {
 
-//         int count = 0;
+  //   int count = 0;
 
-//         for (int i = 0; i < nums.length; i++) {
-//             for (int j = 0; j < nums.length; j++) {
-//                 if (i < j && nums[i] == nums[j]) {
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
+  //   for (int i = 0; i < nums.length; i++) {
+  //     for (int j = 0; j < nums.length; j++) {
+  //       if (i < j && nums[i] == nums[j]) {
+  //         count++;
+  //       }
+  //     }
+  //   }
+  //   return count;
+  // }
 }
