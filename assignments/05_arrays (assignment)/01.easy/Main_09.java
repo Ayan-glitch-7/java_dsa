08e253404dafc32c323d6c3443e3b4efa2d8e842
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-23 19:48:46 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_09.java
+++ assignments/05_arrays (assignment)/01.easy/Main_09.java
@@ -1,16 +1,16 @@
 /* LEETCODE (# 1389) */
 
 class Main_09 {
-    // public int[] createTargetArray(int[] nums, int[] index) {
+  // public int[] createTargetArray(int[] nums, int[] index) {
 
-    //     int[] target = new int[nums.length];
+  //   int[] target = new int[nums.length];
 
-    //     for (int i = 0; i < nums.length; i++) {
-    //         for (int j = nums.length - 1; j > index[i]; j--) {
-    //             target[j] = target[j - 1];
-    //         }
-    //         target[index[i]] = nums[i];
-    //     }
-    //     return target;
-    // }
+  //   for (int i = 0; i < nums.length; i++) {
+  //     for (int j = nums.length - 1; j > index[i]; j--) {
+  //       target[j] = target[j - 1];
+  //     }
+  //     target[index[i]] = nums[i];
+  //   }
+  //   return target;
+  // }
 }
