b59d0b8955de35d3167620d020179565c361a15c
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-24 16:28:21 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_11.java
+++ assignments/05_arrays (assignment)/01.easy/Main_11.java
@@ -3,26 +3,26 @@
 // import java.util.List;
 
 class Main_11 {
-    // public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+  // public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
 
-    //     int count = 0;
-    //     int index = 0;
+  //   int count = 0;
+  //   int index = 0;
 
-    //     if (ruleKey.equals("type")) {
-    //         index = 0;
-    //     } else if (ruleKey.equals("color")) {
-    //         index = 1;
-    //     } else if (ruleKey.equals("name")) {
-    //         index = 2;
-    //     } else {
-    //         index = 3;
-    //     }
+  //   if (ruleKey.equals("type")) {
+  //     index = 0;
+  //   } else if (ruleKey.equals("color")) {
+  //     index = 1;
+  //   } else if (ruleKey.equals("name")) {
+  //     index = 2;
+  //   } else {
+  //     index = 3;
+  //   }
 
-    //     for (int i = 0; i < items.size(); i++) {
-    //         if (items.get(i).get(index).equals(ruleValue)) {
-    //             count++;
-    //         }
-    //     }
-    //     return count;
-    // }
+  //   for (int i = 0; i < items.size(); i++) {
+  //     if (items.get(i).get(index).equals(ruleValue)) {
+  //       count++;
+  //     }
+  //   }
+  //   return count;
+  // }
 }
