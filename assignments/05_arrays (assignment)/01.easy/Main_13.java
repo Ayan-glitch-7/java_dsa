8149ddeb7a6755397648a5fd92a04919bcee135b
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-25 14:13:20 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_13.java
+++ assignments/05_arrays (assignment)/01.easy/Main_13.java
@@ -1,19 +1,19 @@
 /* LEETCODE (# 832) */
 
 class Main_13 {
-    // public int[][] flipAndInvertImage(int[][] image) {
+  // public int[][] flipAndInvertImage(int[][] image) {
 
-    //     int[][] ans = new int[image.length][image[0].length];
+  //   int[][] ans = new int[image.length][image[0].length];
 
-    //     for (int i = 0; i < image.length; i++) {
-    //         for (int j = 0; j < image[i].length; j++) {
-    //             if (image[i][j] == 0) {
-    //                 ans[i][image[i].length - j - 1] = 1;
-    //             } else if (image[i][j] == 1) {
-    //                 ans[i][image[i].length - j - 1] = 0;
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // }
+  //   for (int i = 0; i < image.length; i++) {
+  //     for (int j = 0; j < image[i].length; j++) {
+  //       if (image[i][j] == 0) {
+  //         ans[i][image[i].length - j - 1] = 1;
+  //       } else if (image[i][j] == 1) {
+  //         ans[i][image[i].length - j - 1] = 0;
+  //       }
+  //     }
+  //   }
+  //   return ans;
+  // }
 }
