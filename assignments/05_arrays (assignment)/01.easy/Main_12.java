a95545dd5fe69289a736b7b0f1050ee18aad6586
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-25 14:13:09 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_12.java
+++ assignments/05_arrays (assignment)/01.easy/Main_12.java
@@ -1,22 +1,22 @@
 /* LEETCODE (# 1732) */
 
 class Main_12 {
-    // public int largestAltitude(int[] gain) {
+  // public int largestAltitude(int[] gain) {
 
-    //     int max = 0;
-    //     int prev = 0;
+  //   int max = 0;
+  //   int prev = 0;
 
-    //     int[] ans = new int[gain.length + 1];
+  //   int[] ans = new int[gain.length + 1];
 
-    //     ans[0] = 0;
+  //   ans[0] = 0;
 
-    //     for (int i = 0; i < gain.length; i++) {
-    //         prev = prev + gain[i];
-    //         ans[i] = prev;
-    //         if (ans[i] > max) {
-    //             max = ans[i];
-    //         }
-    //     }
-    //     return max;
-    // }
+  //   for (int i = 0; i < gain.length; i++) {
+  //     prev = prev + gain[i];
+  //     ans[i] = prev;
+  //     if (ans[i] > max) {
+  //       max = ans[i];
+  //     }
+  //   }
+  //   return max;
+  // }
 }
