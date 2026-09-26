7092f530e60dc35d778ed5796713fa49f191b57e
Ayan-glitch-7 <mohdayan7245@gmail.com>
2026-09-26 18:01:53 +0530

    c

--- assignments/05_arrays (assignment)/01.easy/Main_14.java
+++ assignments/05_arrays (assignment)/01.easy/Main_14.java
@@ -1,33 +1,33 @@
 /* LEETCODE (# 1252) */
 
 class Main_14 {
-    // public int oddCells(int m, int n, int[][] indices) {
+  // public int oddCells(int m, int n, int[][] indices) {
 
-    //     int[][] mat = new int[m][n];
-    //     int count = 0;
+  //   int[][] mat = new int[m][n];
+  //   int count = 0;
 
-    //     for (int i = 0; i < indices.length; i++) {
-    //         int row = indices[i][0];
-    //         int col = indices[i][1];
+  //   for (int i = 0; i < indices.length; i++) {
+  //     int row = indices[i][0];
+  //     int col = indices[i][1];
 
-    //         for (int j = 0; j < m; j++) {
-    //             mat[j][col]++;
-    //         }
+  //     for (int j = 0; j < m; j++) {
+  //       mat[j][col]++;
+  //     }
 
-    //         for (int j = 0; j < n; j++) {
-    //             mat[row][j]++;
-    //         }
+  //     for (int j = 0; j < n; j++) {
+  //       mat[row][j]++;
+  //     }
 
-    //     }
+  //   }
 
-    //     for (int j = 0; j < m; j++) {
-    //         for (int k = 0; k < n; k++) {
-    //             if (mat[j][k] % 2 != 0) {
-    //                 count++;
-    //             }
-    //         }
-    //     }
+  //   for (int j = 0; j < m; j++) {
+  //     for (int k = 0; k < n; k++) {
+  //       if (mat[j][k] % 2 != 0) {
+  //         count++;
+  //       }
+  //     }
+  //   }
 
-    //     return count;
-    // }
+  //   return count;
+  // }
 }
